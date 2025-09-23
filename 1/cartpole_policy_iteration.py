a16392d57a59e0b876851a3ec35ae741b167b7f3
invain01 <3038026071@qq.com>
2025-09-23 22:28:38 +0800

    fix.episode上限改为8000

--- 1/cartpole_policy_iteration.py
+++ 1/cartpole_policy_iteration.py
@@ -4,7 +4,7 @@ import time
 import matplotlib.pyplot as plt
 
 class QLearning:
-    def __init__(self, env, bins=(10, 10, 10, 10), gamma=0.99, alpha=0.1, epsilon=0.1, num_episodes=50000):
+    def __init__(self, env, bins=(10, 10, 10, 10), gamma=0.99, alpha=0.1, epsilon=0.1, num_episodes=8000):
         self.env = env
         self.bins = bins
         self.position_bins = np.linspace(-4.8, 4.8, self.bins[0])
@@ -19,33 +19,10 @@ class QLearning:
         self.num_actions = env.action_space.n
         self.Q = np.zeros((self.num_states, self.num_actions))
 
-    def discretize_state(self, state):
-        discretized = [
-            np.clip(np.digitize(state[0], self.position_bins) - 1, 0, self.bins[0] - 1),
-            np.clip(np.digitize(state[1], self.velocity_bins) - 1, 0, self.bins[1] - 1),
-            np.clip(np.digitize(state[2], self.angle_bins) - 1, 0, self.bins[2] - 1),
-            np.clip(np.digitize(state[3], self.angular_velocity_bins) - 1, 0, self.bins[3] - 1)
-        ]
-        return tuple(discretized)
-
-    def state_to_index(self, state):
-        index = 0
-        multiplier = 1
-        for i in range(len(state)):
-            index += state[i] * multiplier
-            multiplier *= self.bins[i]
-        return index
-
-    def choose_action(self, state_idx):
-        if np.random.rand() < self.epsilon:
-            return np.random.randint(self.num_actions)
-        else:
-            return np.argmax(self.Q[state_idx])
-
     def train(self):
         rewards = []
         episodes = []
-        for episode in range(self.num_episodes):
+        for episode in range(self.num_episodes + 1):
             state, _ = self.env.reset()
             state = self.discretize_state(state)
             state_idx = self.state_to_index(state)
@@ -69,25 +46,36 @@ class QLearning:
                 test_env = gym.make('CartPole-v1', render_mode='human')
                 original_env = self.env
                 self.env = test_env
-                #avg_reward = self.test(num_episodes=20, no_render=False, verbose=True)  
-                avg_reward = self.test(num_episodes=20, no_render=True, verbose=True)  
+                avg_reward = self.test(num_episodes=10, no_render=False, verbose=True)
                 rewards.append(avg_reward)
                 episodes.append(episode)
                 print(f"After {episode} episodes, average reward: {avg_reward}")
                 self.env = original_env
                 test_env.close()
-        # 添加5000集的测试
-        print("Episode 5000")
-        test_env = gym.make('CartPole-v1', render_mode='human')
-        original_env = self.env
-        self.env = test_env
-        avg_reward = self.test(num_episodes=20, no_render=False, verbose=True)
-        rewards.append(avg_reward)
-        episodes.append(5000)
-        print(f"After 5000 episodes, average reward: {avg_reward}")
-        self.env = original_env
-        test_env.close()
         return episodes, rewards
+
+    def discretize_state(self, state):
+        discretized = [
+            np.clip(np.digitize(state[0], self.position_bins) - 1, 0, self.bins[0] - 1),
+            np.clip(np.digitize(state[1], self.velocity_bins) - 1, 0, self.bins[1] - 1),
+            np.clip(np.digitize(state[2], self.angle_bins) - 1, 0, self.bins[2] - 1),
+            np.clip(np.digitize(state[3], self.angular_velocity_bins) - 1, 0, self.bins[3] - 1)
+        ]
+        return tuple(discretized)
+
+    def state_to_index(self, state):
+        index = 0
+        multiplier = 1
+        for i in range(len(state)):
+            index += state[i] * multiplier
+            multiplier *= self.bins[i]
+        return index
+
+    def choose_action(self, state_idx):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.num_actions)
+        else:
+            return np.argmax(self.Q[state_idx])
 
     def get_policy(self):
         return np.argmax(self.Q, axis=1)
@@ -125,7 +113,7 @@ class QLearning:
 
 if __name__ == "__main__":
     env_train = gym.make('CartPole-v1')
-    ql = QLearning(env_train, num_episodes=5000)
+    ql = QLearning(env_train, num_episodes=8000)
     episodes, rewards = ql.train()
     
     # 绘制图像
